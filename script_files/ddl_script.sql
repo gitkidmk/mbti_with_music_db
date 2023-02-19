40eb8bb91003a5f5cf0c11173b50d099784a1eba
gitkidmk <lovekangmk@gmail.com>
2023-02-19 14:54:20 +0900

    [feat] add session storage info

--- script_files/ddl_script.sql
+++ script_files/ddl_script.sql
@@ -1,5 +1,31 @@
 use mbti_music;
 
+-- mbti_music.SPRING_SESSION definition
+
+CREATE TABLE `SPRING_SESSION` (
+  `PRIMARY_ID` char(36) NOT NULL,
+  `SESSION_ID` char(36) NOT NULL,
+  `CREATION_TIME` bigint(20) NOT NULL,
+  `LAST_ACCESS_TIME` bigint(20) NOT NULL,
+  `MAX_INACTIVE_INTERVAL` int(11) NOT NULL,
+  `EXPIRY_TIME` bigint(20) NOT NULL,
+  `PRINCIPAL_NAME` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`PRIMARY_ID`),
+  UNIQUE KEY `SPRING_SESSION_IX1` (`SESSION_ID`),
+  KEY `SPRING_SESSION_IX2` (`EXPIRY_TIME`),
+  KEY `SPRING_SESSION_IX3` (`PRINCIPAL_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- mbti_music.SPRING_SESSION_ATTRIBUTES definition
+
+CREATE TABLE `SPRING_SESSION_ATTRIBUTES` (
+  `SESSION_PRIMARY_ID` char(36) NOT NULL,
+  `ATTRIBUTE_NAME` varchar(200) NOT NULL,
+  `ATTRIBUTE_BYTES` blob NOT NULL,
+  PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`),
+  CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `SPRING_SESSION` (`PRIMARY_ID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
 -- mbti_music.mbti_music_user definition
 
 CREATE TABLE `mbti_music_user` (
@@ -10,7 +36,7 @@ CREATE TABLE `mbti_music_user` (
   `session_id` varchar(1000) DEFAULT NULL,
   `mbti_music_user_id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`mbti_music_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.mbti_question definition
@@ -22,7 +48,7 @@ CREATE TABLE `mbti_question` (
   `contents` varchar(200) NOT NULL,
   `question_set` char(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.mbti_result definition
@@ -36,7 +62,7 @@ CREATE TABLE `mbti_result` (
   `JP` float DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   PRIMARY KEY (`result_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.music definition
@@ -48,7 +74,8 @@ CREATE TABLE `music` (
   `music_name` varchar(1000) DEFAULT NULL,
   `thumbnail` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`music_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 
 
 -- mbti_music.mbti_music_great definition
@@ -61,7 +88,7 @@ CREATE TABLE `mbti_music_great` (
   PRIMARY KEY (`mbti_music_great_id`),
   KEY `FK_music_TO_mbti_music_great_2` (`music_id`),
   CONSTRAINT `FK_music_TO_mbti_music_great_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.music_mbti_rel definition
@@ -73,4 +100,4 @@ CREATE TABLE `music_mbti_rel` (
   PRIMARY KEY (`music_mbti_id`),
   KEY `FK_music_TO_music_mbti_rel_1` (`music_id`),
   CONSTRAINT `FK_music_TO_music_mbti_rel_1` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
