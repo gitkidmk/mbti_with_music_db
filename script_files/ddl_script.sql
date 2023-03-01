0f812277cb9466a6aa8191995cc8eacbbe89b0ff
강민경 <minkyeong_kang@tmax.co.kr>
2023-03-01 17:14:46 +0900

    [refactor] 불필요한 TB 삭제 및 column 수정

--- script_files/ddl_script.sql
+++ script_files/ddl_script.sql
@@ -26,18 +26,6 @@ CREATE TABLE `SPRING_SESSION_ATTRIBUTES` (
   CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `SPRING_SESSION` (`PRIMARY_ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
--- mbti_music.mbti_music_user definition
-
-CREATE TABLE `mbti_music_user` (
-  `music_id` varchar(1000) NOT NULL,
-  `mbti_name` char(4) DEFAULT NULL,
-  `album` varchar(1000) DEFAULT NULL,
-  `artist` varchar(1000) DEFAULT NULL,
-  `session_id` varchar(1000) DEFAULT NULL,
-  `mbti_music_user_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`mbti_music_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
 
 -- mbti_music.mbti_question definition
 
@@ -48,7 +36,7 @@ CREATE TABLE `mbti_question` (
   `contents` varchar(200) NOT NULL,
   `question_set` char(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.mbti_result definition
@@ -62,19 +50,19 @@ CREATE TABLE `mbti_result` (
   `JP` float DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   PRIMARY KEY (`result_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 
 -- mbti_music.music definition
 
 CREATE TABLE `music` (
   `music_id` varchar(1000) NOT NULL,
-  `album` varchar(1000) DEFAULT NULL,
-  `artist` varchar(1000) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
   `music_name` varchar(1000) DEFAULT NULL,
   `thumbnail` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`music_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 
 
 
@@ -88,16 +76,4 @@ CREATE TABLE `mbti_music_great` (
   PRIMARY KEY (`mbti_music_great_id`),
   KEY `FK_music_TO_mbti_music_great_2` (`music_id`),
   CONSTRAINT `FK_music_TO_mbti_music_great_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
-
--- mbti_music.music_mbti_rel definition
-
-CREATE TABLE `music_mbti_rel` (
-  `music_mbti_id` int(11) NOT NULL AUTO_INCREMENT,
-  `music_id` varchar(1000) NOT NULL,
-  `mbti_name` char(4) DEFAULT NULL,
-  PRIMARY KEY (`music_mbti_id`),
-  KEY `FK_music_TO_music_mbti_rel_1` (`music_id`),
-  CONSTRAINT `FK_music_TO_music_mbti_rel_1` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
